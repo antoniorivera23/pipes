6803c8ce7a4a249c542a350809929354b875bf06
Antonio <antonio.023@hotmail.com>
2021-03-03 19:23:28 -0600

    Subiendo archivos pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+ nombre: string = 'Spiderman';
+ nombre2:string = 'antOn1O riVerA'
+ personas = ['Antonio', 'Daniel', 'Marco', 'Polo', 'Roberto', 'Miguel', 'John', 'Carlos', 'Erick', 'Silvestre'];
+ PI:number = Math.PI;
+ porcentaje: number = 0.234;
+ salario  :number = 1234.5;
+ fecha    :Date = new Date();
+ idioma   :string = 'es';
+ videoUrl :string = 'https://www.youtube.com/embed/tkPhHro64dU';
+ activar : boolean = true;
+
+ valorPromesa = new Promise<string>( (resolve) =>{
+   setTimeout(() => {
+     resolve('llego la data');
+   }, 4500);
+ });
+
+ heroe = {
+   nombre: 'Logan',
+   clave: 'Wolverine',
+   edad: 500,
+   direccion: {
+     calle: 'Primera',
+     casa: 20
+   }
+ }
+
 }
